47ecc786e640cb324abb5c94308097eb817e473c
ian <yihengz@outlook.com>
2025-04-06 07:42:10 +0800

    optionalDependencies

--- makeCore.ts
+++ makeCore.ts
@@ -11,21 +11,17 @@ export async function makeCore() {
   pkg.files = ['*.d.ts', '*.js', '*.mjs'];
   pkg.repository = {
     type: 'git',
-    url: 'git+https://github.com/ianzone/uws.git',
+    url: 'git+https://github.com/ianzone/uwsjs.git',
   };
   pkg.scripts = {
     release: 'bun publish --access public',
   };
-  pkg.peerDependencies = {};
-  pkg.peerDependenciesMeta = {};
+  pkg.optionalDependencies = {};
   const binPaths = (await run(`ls ${coreDir}/uws_*.node`)).stdout.split('\n');
   for (const binPath of binPaths) {
     const peer = await makeBin(binPath, pkg.version as string);
     await Bun.write(`${binPath}.js`, `module.exports = require('${peer}');`);
-    pkg.peerDependencies[peer] = pkg.version;
-    pkg.peerDependenciesMeta[peer] = {
-      optional: true,
-    };
+    pkg.optionalDependencies[peer] = pkg.version;
   }
 
   await Bun.write(pkgJsonDir, JSON.stringify(pkg, null, 2));
